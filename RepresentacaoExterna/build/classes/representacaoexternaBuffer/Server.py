28dc1e6e13f445e715ff6630a27cc9733dfff7df
Elaine Sangali <e.nani92@gmail.com>
2018-10-29 08:45:08 -0300

    Arrumando import do protobuf

--- RepresentacaoExterna/build/classes/representacaoexternaBuffer/Server.py
+++ RepresentacaoExterna/build/classes/representacaoexternaBuffer/Server.py
@@ -1,7 +1,7 @@
 import socket
-from Gerenciamento_pb2.py import Gerenciamento
+from Gerenciamento_pb2 import Gerenciamento 
 
-g = Gerenciamento_pb2.Gerenciamento()
+g = Gerenciamento()
 print("Executando...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 5555))
